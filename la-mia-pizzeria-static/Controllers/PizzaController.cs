e9588eddf850959d7c511e9d58a7991947e9dc64
Fesposito9197 <kekkoesp8@gmail.com>
2023-04-11 14:16:18 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,44 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using var ctx = new PizzeriaContext();
+
+            var pizzas = ctx.Pizzas.ToArray();
+            return View(pizzas);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            using var ctx = new PizzeriaContext();
+
+           var pizza = ctx.Pizzas.Single(p => p.Id == id);
+
+           return View(pizza);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/PizzeriaContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LocalHost;Initial Catalog=PizzeriaDb;Integrated Security=True;Pooling=False;Encrypt=False;");
+        }
+        public void seed() 
+        {
+            if (!Pizzas.Any())
+            {
+                var seed = new Pizza[]
+                {
+                    new Pizza
+                    {
+                        Name = "Margherita",
+                        Description = "La pizza Margherita è la tipica pizza napoletana, condita con pomodoro, mozzarella, basilico fresco, sale e olio;",
+                        Foto = "/img/pizza-napoletana.jpg",
+                        Price = 5,
+                    },
+                    new Pizza
+                    {
+                        Name = "Marinara",
+                        Description = "La pizza alla marinara è una tipica pizza napoletana condita con pomodoro, aglio, origano, olio.",
+                        Foto = "/img/Pizza_marinara.jpg",
+                        Price = 4,
+                    },
+                    new Pizza
+                    {
+                        Name = "Capricciosa",
+                        Description = "La pizza capricciosa è una pizza tipica della cucina italiana caratterizzata da un condimento di pomodoro, mozzarella, prosciutto cotto, funghi, olive verdi e nere, e carciofini e spesso uova.",
+                        Foto ="/img/Pizza_capricciosa.jpg",
+                        Price = 8,
+                    },
+                    new Pizza
+                    {
+                        Name = "Diavola",
+                        Description = "La pizza diavola è una variazione della pizza margherita con l'aggiunta di salame piccante.",
+                        Foto = "/img/pizza_diavola.jpg",
+                        Price = 7,
+                    }
+                };
+                Pizzas.AddRange(seed);
+
+                SaveChanges();
+            }
+        }
+    }
+}
